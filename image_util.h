1bf120afd2fa31c97ff35d19bbb5bca514be0dcd
Jake Naviasky <jnaviask@andrew.cmu.edu>
2013-01-14 00:16:15 +0000

    Removing image.h

--- image_util.h
+++ image_util.h
@@ -1,12 +1,32 @@
 #ifndef IMAGE_UTIL_
 #define IMAGE_UTIL_
 
-#include "image.h"
-#include "lib/uba.h"
-
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
 #include <dirent.h>
 #include <string.h>
 #include <math.h>
+
+#include "lib/uba.h"
+#include "lib/xalloc.h"
+#include "lib/contracts.h"
+#include "lib/stb_image.c"
+
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include "lib/stb_image_write.h"
+
+typedef uint32_t pixel;
+typedef unsigned char rgb;
+
+struct img_t {
+  uint32_t w;
+  uint32_t h;
+  pixel *data;
+};
+
+typedef struct img_t img;
 
 struct img_data_t {
   pixel *avg;
